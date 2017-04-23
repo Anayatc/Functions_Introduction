c29d80d938cdca81c64485c01a152def8e514c48
Anayatc <anayatc@sky.com>
2017-04-23 15:40:52 +0100

    Take a test

--- ex26.py
+++ ex26.py
@@ -82,8 +82,8 @@ print("We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point
 
 
 sentence = "All good things come to those who wait."
-
 words = break_words(sentence)
+
 sorted_words = sort_words(words)
 
 print_first_word(words)
@@ -92,7 +92,5 @@ print_first_word(sorted_words)
 print_last_word(sorted_words)
 sorted_words = sort_sentence(sentence)
 print(sorted_words)
-
 print_first_and_last(sentence)
-
 print_first_and_last_sorted(sentence)
